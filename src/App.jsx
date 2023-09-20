6bc464aecd36527a4f4f8d7c37f17b70d2942771
Rafael S <rafael_soares007@uni9.edu.br>
2023-09-19 21:50:55 -0300

    Adicionando rotas e paginas

--- src/App.jsx
+++ src/App.jsx
@@ -1,65 +1,15 @@
 import { Header } from "./components/Header";
 import "./App.css";
-import { Contador } from "./components/Contador";
+import Router from "./Router";
 
 
-function App() {
-  const listas = [
-    {
-      nome: "Projeto Novo",
-      responsavel: "Fernando Carlos",
-      valor: 1200,
-
-  },
-  {
-    nome: "Projeto Mar",
-    responsavel: "Fonseca",
-    valor: 4000,
-
-},
-];
-
-console.log(listas); 
-
-  return (
+function App(){
+    return (
    <div className="container">
       <Header title="Projeto Vision do Rafael" />
-    <h1>
-      Conteúdo do projeto vision
-    </h1>
-    
-    <main>
-      <section>
-        <img src="https://fastly.picsum.photos/id/1002/300/300.jpg?hmac=BoX0yYHGnuaryVR22pkiNUrVS3oOMU1nL4i3bObV6A4" alt="" />
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime reiciendis odio doloribus impedit nobis obcaecati, omnis iste sit pariatur quas. Exercitationem saepe, necessitatibus quis vero illo distinctio quas aliquid magnam! 
-        </p>
-      </section>
-
-      <section>
-        <h1>
-          Listas de projetos
-        </h1>
-
-        <article className="listas">
-           
-          {listas.map((item,index) =>{
-            
-            return(
-            <div key={index}>
-            <strong>Nome: </strong> <span>{item.nome}</span>
-            <strong>Responsável: </strong> <span>{item.responsavel}</span>
-            <strong>Valor: </strong> <span>{item.valor}</span>
-          </div>
-            )
-
-          })}
-        </article>
-      </section>
-    </main>
-    <Contador />
+      <Router/>
    </div>
-  )
+  );
 }
 
-export default App
+export default App;
